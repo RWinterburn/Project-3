1c2e837c9585d8517bb54c8b256cefecc1ad7038
Rob Winterburn <forestalwinters@gmail.com>
2024-08-01 13:55:07 +0000

    imported request, flash messages, redirect, url for and session

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from models import User
 from app import db
 
@@ -6,4 +6,9 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
