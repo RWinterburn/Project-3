ae9f8c93d2118740290d4a3f59deee9e6de8fecb
Rob Winterburn <forestalwinters@gmail.com>
2024-08-01 13:35:14 +0000

    ADD started adding auth routes for login

--- app/auth.py
+++ app/auth.py
@@ -1,1 +1,9 @@
 from flask import Blueprint, render_template
+from models import User
+from app import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method
