388009e8586baa809c3bddb663c600cdaecaea67
Rob Winterburn <forestalwinters@gmail.com>
2024-09-05 12:30:07 +0000

    added to readme and deployed

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # Use the port assigned by Heroku, default to 5000 for local development
+
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
